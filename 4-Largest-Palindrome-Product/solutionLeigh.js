1ee9019afeaae56b3d5a4c06590be03c64cff8ea
leighembee <leigh.blechman@gmail.com>
2018-02-24 20:42:23 -0500

    palindrome helper function added

--- 4-Largest-Palindrome-Product/solutionLeigh.js
+++ 4-Largest-Palindrome-Product/solutionLeigh.js
@@ -1,1 +1,22 @@
+/*
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+*/
+
+function largestThreeDigitPalindromicNumber() {
+// start by checking with the largest 3 digit numbers and going down
+
+}
+
+
+
+// check if palindrome helper function
+function isPalindrome(num) {
+  let numString = String(num).split('').join('')
+  let reverse = String(num).split('').reverse().join('')
+
+return numString === reverse
+
+}
 
