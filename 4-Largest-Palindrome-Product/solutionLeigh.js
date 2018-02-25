759b13b87ea3fc3eb79d828ec75b406c8d8d6a5e
leighembee <leigh.blechman@gmail.com>
2018-02-24 21:14:09 -0500

    refactored

--- 4-Largest-Palindrome-Product/solutionLeigh.js
+++ 4-Largest-Palindrome-Product/solutionLeigh.js
@@ -4,30 +4,18 @@ A palindromic number reads the same both ways. The largest palindrome made from 
 Find the largest palindrome made from the product of two 3-digit numbers.
 */
 
-function largestThreeDigitPalindromicNumber() {
-// start by checking with the largest 3 digit numbers and going down
+const largestThreeDigitPalindromicNumber = () => {
 
   let largest = 0
 
   for (i = 999; i > 99; i--) {
-    for (j=999; j > 99; j--) {
+    for (j = 999; j > 99; j--) {
       let currentProduct = i * j
-      let string = '' * currentProduct
-      if (isPalindrome(currentProduct) && currentProduct > largest) {
+      let rString = String(currentProduct).split('').reverse().join('')
+      if (rString === String(currentProduct).split('').join('') && currentProduct > largest) {
         largest = currentProduct
       }
     }
   }
   return largest
 }
-
-
-// check if palindrome helper function
-function isPalindrome(num) {
-  let numString = String(num).split('').join('')
-  let reverse = String(num).split('').reverse().join('')
-
-return numString === reverse
-
-}
-console.log(largestThreeDigitPalindromicNumber())
